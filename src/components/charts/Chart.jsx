9b34a00027470b87daaa7ec8924ffd0cd677f56d
jaffasunny <jaffer.sunny125@gmail.com>
2022-02-12 14:04:36 +0500

    Working on Charts Component

---
+++ src/components/charts/Chart.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./chart.css";
+
+const Chart = () => {
+	return <div className="chart">Chart</div>;
+};
+
+export default Chart;
--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Chart from "../../charts/Chart";
 import FeaturedInfo from "../../featuredInfo/FeaturedInfo";
 import "./home.css";
 
@@ -6,6 +7,7 @@ const Home = () => {
 	return (
 		<div className="home">
 			<FeaturedInfo />
+			<Chart />
 		</div>
 	);
 };
